91fc04b2276655f3782111109f647377ef21ed4a
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-13 23:05:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver
+  
+  def initialize
+    @sender = sender
+    @receiver = receiver
+    @transfer_amount = transfer_amount
+    @status = "pending"
+  end
 end
