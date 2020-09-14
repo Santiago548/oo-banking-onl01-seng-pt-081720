491d09d37ddad6bf7a27338829014ff5beefa9da
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-14 00:19:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
-           
+
     else
       "Transaction rejected. Please check your account balance."
     end
